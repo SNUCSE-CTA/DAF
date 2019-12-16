4ef659b940e89a1f861bdc552f74a02b64975192
Kim Jongjin <j2kim99@paran.com>
2019-12-17 03:21:32 +0900

    log/n dup num huristic

--- src/minDup.java
+++ src/minDup.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.math.*;
 
 class main{
     public static void main(String[] args){
@@ -68,7 +69,7 @@ public class minDup{
     }
 
     //downOrder는 인접리스트와 차수, 정점의 수를 받아 차수의 내림차순 정렬을 리턴한다.
-    //리턴 배열 a[i]는 입력의 i번째 정점이 몇 번 정점이어야 하는지이다.
+    //리턴 배열 a[i]는 적합하게 재배열된 배열이다.
     //i는 0에서 시작한다.
     public int[] downOrder(int[][] adjList, int degree[], int N){
         int[] rtn = new int[N];
@@ -106,6 +107,69 @@ public class minDup{
         return rtn;
     }
 
+    // 그리디스럽게 정점을 0번부터 보면서 어디에 넣으면 중복 정점이 적을지 봐보자.
+    // 패스트리 겹치는 정점 줄이기 이슈의 4번 코멘트 참조
+    // 입력은 인접리스트, 차수, 크기
+    public int[] greedyLog(int[][] adjList, int[] degree, int N){
+        int[] dup = new int[N];
+        double[] logdup = new double[N];
+        int[] arr = new int[N];
+        int[] where = new int[N];
+        arr[0] = 0;
+        where[0] = 0;
+        double sum;
 
+        for(int v = 1; v < N; v++){
+            dup = new int[N];
+            for(int i = 0; i < v; i++){
+                for(int j = 0; j < degree[arr[i]]; j++){
+                    if(adjList[arr[i]][j] >= v) continue;
+                    else if(where[adjList[arr[i]][j]] < where[arr[i]]){
+                        dup[arr[i]] += dup[adjList[arr[i]][j]];
+                    }
+                }
+                if(dup[arr[i]] == 0)
+                    dup[arr[i]] = 1;
+            }
+            for(int i = 0; i < v; i++){
+                logdup[i] = Math.log(dup[i])/(where[i]+1);
+            }
 
+            sum = 0;
+            for(int i = 0; i < degree[v]; i++){
+                sum += logdup[adjList[v][i]];
+            }
+            sum /= 2;
+            double nujuckhap = 0;
+            int a=0, b=0;
+            for(int loc = 0; loc <= v; loc++){
+                boolean chk = false;
+                for(int i = 0; i < degree[v]; i++){
+                    if(arr[loc] == adjList[v][i]){
+                        chk = true;
+                        break;
+                    }
+                }
+                if(chk){
+                    nujuckhap+=dup[arr[loc]];
+                    if(nujuckhap<sum) a = loc;
+                    else{
+                        b = loc;
+                        break;
+                    }
+                }
+            }
+            a = (a+b+1)/2;
+            where[v] = a;
+            for(int i = 0; i < v; i++) {
+                if (where[i] >= a) where[i]++;
+            }
+            for(int i = v; i>a; i--){
+                arr[i] = arr[i-1];
+            }
+            arr[a] = v;
+        }
+
+        return arr;
+    }
 }
