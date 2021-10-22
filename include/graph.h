e85ba57835625aa4276dc065b67fef5be41efb8f
jihoonjang <ljjk159@gmail.com>
2021-10-22 15:23:08 +0900

    graph.h skeleton added

---
+++ include/graph.h
@@ -0,0 +1,78 @@
+#ifndef GRAPH_H_
+#define GRAPH_H_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "global/global.h"
+
+namespace daf {
+
+class Graph {
+ public:
+  Graph();
+  ~Graph();
+
+  Graph& operator=(const Graph&) = delete;
+  Graph(const Graph&) = delete;
+
+  inline Size GetNumLabels() const;
+  inline Size GetNumVertices() const;
+  inline Size GetNumEdges() const;
+  inline Size GetMaxDegree() const;
+
+  inline Label GetLabel(Vertex v) const;
+  inline Size GetStartOffset(Vertex v) const;
+  inline Size GetEndOffset(Vertex v) const;
+  inline Size GetDegree(Vertex v) const;
+  inline Size GetCoreNum(Vertex v) const;
+
+  inline Label GetLabelFrequency(Label l) const;
+
+  inline Vertex GetNeighbor(Size i) const;
+
+ protected:
+  void LoadRoughGraph(const std::string& filename,
+                      std::vector<std::vector<Vertex>>* graph);
+  void computeCoreNum();
+
+  Size num_vertex_;
+  Size num_edge_;
+  Size num_label_;
+
+  Size max_degree_;
+
+  Label* label_;
+  Size* start_off_;
+  Vertex* linear_adj_list_;
+  Size* label_frequency_;
+
+  Size* core_num_;
+};
+
+inline Size Graph::GetNumLabels() const { /* code */ }
+
+inline Size Graph::GetNumVertices() const { /* code */ }
+
+inline Size Graph::GetNumEdges() const { /* code */ }
+
+inline Size Graph::GetMaxDegree() const { /* code */ }
+
+inline Label Graph::GetLabel(Vertex v) const { /* code */ }
+
+inline Size Graph::GetStartOffset(Vertex v) const { /* code */ }
+
+inline Size Graph::GetEndOffset(Vertex v) const { /* code */ }
+
+inline Size Graph::GetDegree(Vertex v) const { /* code */ }
+
+inline Size Graph::GetCoreNum(Vertex v) const { /* code */ }
+
+inline Label Graph::GetLabelFrequency(Label l) const { /* code */ }
+
+inline Vertex Graph::GetNeighbor(Size i) const { /* code */ }
+
+}  // namespace daf
+
+#endif  // GRAPH_H_
