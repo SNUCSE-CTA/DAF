fc7f36eb2f0661bc49d91455cd931a348eef85e2
jihoonjang <ljjk159@gmail.com>
2021-10-26 10:51:03 +0900

    GetEndOffset implemented

--- include/graph.h
+++ include/graph.h
@@ -63,7 +63,7 @@ inline Label Graph::GetLabel(Vertex v) const { return label_[v]; }
 
 inline Size Graph::GetStartOffset(Vertex v) const { return start_off_[v]; }
 
-inline Size Graph::GetEndOffset(Vertex v) const { /* code */ }
+inline Size Graph::GetEndOffset(Vertex v) const { return start_off_[v + 1]; }
 
 inline Size Graph::GetDegree(Vertex v) const { /* code */ }
 
