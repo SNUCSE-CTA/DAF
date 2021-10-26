bdfe30418225c84bd7f9b51f79f0cc0e2e5d5665
jihoonjang <ljjk159@gmail.com>
2021-10-26 19:41:19 +0900

    graph.cc skeleton added

---
+++ src/graph.cc
@@ -0,0 +1,15 @@
+#include "include/graph.h"
+
+#include <fstream>
+#include <iostream>
+
+namespace daf {
+Graph::Graph() { /* code */ }
+
+Graph::~Graph() { /* code */ }
+
+void Graph::LoadRoughGraph(const std::string &filename,
+                           std::vector<std::vector<Vertex>> *graph) { /* code */ }
+
+void Graph::computeCoreNum() { /* code */ }
+}  // namespace daf
