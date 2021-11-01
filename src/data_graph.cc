038de67d24a185b1f4dad5cf10692b7b363bb1fb
jihoonjang <ljjk159@gmail.com>
2021-11-01 20:22:32 +0900

    data_graph.cc skeleton added

---
+++ src/data_graph.cc
@@ -0,0 +1,13 @@
+#include "include/data_graph.h"
+
+#include <algorithm>
+#include <unordered_map>
+#include <vector>
+
+namespace daf {
+DataGraph::DataGraph() { /* code */ }
+
+DataGraph::~DataGraph() { /* code */ }
+
+void DataGraph::LoadAndProcessGraph(const std::string& filename) { /* code */ }
+}  // namespace daf
