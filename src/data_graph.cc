502336806d1e603fd57fdf04d89b811f938f58c1
jihoonjang <ljjk159@gmail.com>
2021-11-03 10:39:26 +0900

    ~DataGraph implemented

--- src/data_graph.cc
+++ src/data_graph.cc
@@ -7,7 +7,14 @@
 namespace daf {
 DataGraph::DataGraph() {}
 
-DataGraph::~DataGraph() { /* code */ }
+DataGraph::~DataGraph() {
+  delete[] transferred_label_;
+  delete[] adj_offs_by_label_;
+  delete[] offs_by_label_;
+  delete[] vertices_sorted_;
+  delete[] linear_nbr_bitset_;
+  delete[] max_nbr_degree_;
+}
 
 void DataGraph::LoadAndProcessGraph(const std::string& filename) { /* code */ }
 }  // namespace daf
