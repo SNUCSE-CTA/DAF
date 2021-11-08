eff8ba86ebab8bec09c467e5ffc8ba4102d47133
jihoonjang <ljjk159@gmail.com>
2021-11-08 19:45:31 +0900

    dag.h skeleton added

---
+++ include/dag.h
@@ -0,0 +1,51 @@
+#ifndef DAG_H_
+#define DAG_H_
+
+#include "global/global.h"
+#include "include/data_graph.h"
+#include "include/query_graph.h"
+
+namespace daf {
+class DAG {
+ public:
+  DAG(const DataGraph &data, const QueryGraph &query);
+  ~DAG();
+
+  DAG &operator=(const DAG &) = delete;
+  DAG(const DAG &) = delete;
+
+  void BuildDAG();
+
+  inline Vertex GetRoot() const;
+  inline Size GetNumChildren(Vertex v) const;
+  inline Size GetNumParents(Vertex v) const;
+  inline Size GetChild(Vertex v, Size i) const;
+  inline Size GetParent(Vertex v, Size i) const;
+  inline Size GetInitCandSize(Vertex v) const;
+  inline Vertex GetVertexOrderedByBFS(Size i) const;
+
+ private:
+  const DataGraph &data_;
+  const QueryGraph &query_;
+
+  Vertex *bfs_sequence_;
+  Size *num_children_;
+  Size *num_parents_;
+  Vertex **children_;
+  Vertex **parents_;
+  Size *init_cand_size_;
+
+  Vertex SelectRootVertex();
+};
+
+inline Vertex DAG::GetRoot() const { /* code */ }
+inline Size DAG::GetNumChildren(Vertex v) const { /* code */ }
+inline Size DAG::GetNumParents(Vertex v) const { /* code */ }
+inline Size DAG::GetChild(Vertex v, Size i) const { /* code */ }
+inline Size DAG::GetParent(Vertex v, Size i) const { /* code */ }
+inline Size DAG::GetInitCandSize(Vertex v) const { /* code */ }
+inline Vertex DAG::GetVertexOrderedByBFS(Size i) const { /* code */ }
+
+}  // namespace daf
+
+#endif  // DAG_H_
