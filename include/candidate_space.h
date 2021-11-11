83ad661e4320ea91778caf237ea643cf66c4e5c8
jihoonjang <ljjk159@gmail.com>
2021-11-11 15:28:23 +0900

    candidate_space.h skeleton added

---
+++ include/candidate_space.h
@@ -0,0 +1,72 @@
+#ifndef CANDIDATE_SPACE_H_
+#define CANDIDATE_SPACE_H_
+
+#include <utility>
+
+#include "global/global.h"
+#include "include/dag.h"
+#include "include/data_graph.h"
+#include "include/query_graph.h"
+
+namespace daf {
+class CandidateSpace {
+ public:
+  CandidateSpace(const DataGraph &data, const QueryGraph &query,
+                 const DAG &dag);
+  ~CandidateSpace();
+
+  CandidateSpace &operator=(const CandidateSpace &) = delete;
+  CandidateSpace(const CandidateSpace &) = delete;
+
+  bool BuildCS();
+
+  inline Size GetCandidateSetSize(Vertex u) const;
+  inline Vertex GetCandidate(Vertex u, Size v_idx) const;
+
+  inline Size GetCandidateStartOffset(Vertex u, Size u_adj_idx,
+                                      Size v_idx) const;
+  inline Size GetCandidateEndOffset(Vertex u, Size u_adj_idx, Size v_idx) const;
+  inline Size GetCandidateIndex(Size idx) const;
+
+ private:
+  const DataGraph &data_;
+  const QueryGraph &query_;
+  const DAG &dag_;
+
+  Size *candidate_set_size_;
+  Vertex **candidate_set_;
+  Size **candidate_offsets_;
+  Vertex *linear_cs_adj_list_;
+
+  Size *num_visit_cs_;
+  Vertex *visited_candidates_;
+  Size *cand_to_cs_idx_;
+  Size num_visitied_candidates_;
+
+  bool FilterByTopDownWithInit();
+  bool FilterByBottomUp();
+  bool FilterByTopDown();
+
+  void ConstructCS();
+  bool InitRootCandidates();
+  void AllocateSpaceForCS();
+
+  void ComputeNbrInformation(Vertex u, Size *max_nbr_degree,
+                             uint64_t *label_set);
+};
+
+inline Size CandidateSpace::GetCandidateSetSize(Vertex u) const { /* code */ }
+
+inline Vertex CandidateSpace::GetCandidate(Vertex u, Size v_idx) const { /* code */ }
+
+inline Size CandidateSpace::GetCandidateStartOffset(Vertex u, Size u_adj_idx,
+                                                    Size v_idx) const { /* code */ }
+
+inline Size CandidateSpace::GetCandidateEndOffset(Vertex u, Size u_adj_idx,
+                                                  Size v_idx) const { /* code */ }
+
+inline Size CandidateSpace::GetCandidateIndex(Size idx) const { /* code */ }
+
+}  // namespace daf
+
+#endif  // CANDIDATE_SPACE_H_
