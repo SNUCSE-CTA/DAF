bab49fb501b76c83f4273625f3699862234d18da
jihoonjang <ljjk159@gmail.com>
2021-11-16 12:26:48 +0900

    Ordering implemented

--- src/ordering.cc
+++ src/ordering.cc
@@ -2,7 +2,13 @@
 
 namespace daf {
 Ordering::Ordering(Size num_query_vertices)
-    : num_query_vertices_(num_query_vertices) { /* code */ }
+    : num_query_vertices_(num_query_vertices) {
+  extendable_queue_ = new Vertex[num_query_vertices_];
+  extendable_queue_size_ = 0;
+  weights_ = new Size[num_query_vertices_];
+  std::fill(weights_, weights_ + num_query_vertices_,
+            std::numeric_limits<Size>::max());
+}
 
 Ordering::~Ordering() { /* code */ }
 
