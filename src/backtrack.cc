380d2fa8608a3fa339b80c5d547b3f12b82c7057
jihoonjang <ljjk159@gmail.com>
2021-11-19 12:32:26 +0900

    Backtrack implemented

--- src/backtrack.cc
+++ src/backtrack.cc
@@ -5,7 +5,32 @@
 namespace daf {
 Backtrack::Backtrack(const DataGraph &data, const QueryGraph &query,
                      const CandidateSpace &cs)
-    : data_(data), query_(query), cs_(cs) { /* code */ }
+    : data_(data), query_(query), cs_(cs) {
+  num_embeddings_ = 0;
+  num_backtrack_calls_ = 0;
+  backtrack_depth_ = 0;
+
+  mapped_query_vtx_ = new Vertex[data_.GetNumVertices()];
+  node_stack_ = new SearchTreeNode[query_.GetNumNonLeafVertices() + 1];
+  mapped_nodes_ = new SearchTreeNode *[query_.GetNumVertices()];
+  helpers_ = new BacktrackHelper[query_.GetNumVertices()];
+
+  extendable_queue_ = new Ordering(query_.GetNumVertices());
+
+  if (query_.GetNumNonLeafVertices() < query_.GetNumVertices()) {
+    match_leaves_ = new MatchLeaves(data_, query_, cs_, mapped_query_vtx_,
+                                    helpers_, mapped_nodes_);
+  } else {
+    match_leaves_ = nullptr;
+  }
+
+  std::fill(mapped_query_vtx_, mapped_query_vtx_ + data_.GetNumVertices(), -1);
+
+  for (Vertex u = 0; u < query_.GetNumVertices(); ++u) {
+    helpers_[u].Initialize(query_.GetNumVertices(), query_.GetDegree(u),
+                           cs_.GetCandidateSetSize(u), u);
+  }
+}
 
 Backtrack::~Backtrack() { /* code */ }
 
