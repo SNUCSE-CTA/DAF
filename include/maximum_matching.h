758eb6518490137d8c38f32c3dc5bedf0589ddf4
jihoonjang <ljjk159@gmail.com>
2021-11-23 10:56:59 +0900

    maximum_matching.h skeleton added

---
+++ include/maximum_matching.h
@@ -0,0 +1,86 @@
+#ifndef MAXIMUM_MATCHING_H_
+#define MAXIMUM_MATCHING_H_
+
+#include <limits>
+
+#include "global/global.h"
+#include "include/backtrack_helper.h"
+#include "include/candidate_space.h"
+#include "include/data_graph.h"
+#include "include/query_graph.h"
+
+namespace daf {
+class MaximumMatching {
+ public:
+  inline MaximumMatching(const DataGraph &data, const QueryGraph &query,
+                         const CandidateSpace &cs,
+                         BacktrackHelper *backtrack_helpers);
+  inline ~MaximumMatching();
+
+  MaximumMatching &operator=(const MaximumMatching &) = delete;
+  MaximumMatching(const MaximumMatching &) = delete;
+
+  inline Size ComputeMaximumMatching(Label label);
+
+  inline void AddToU(Size u);
+  inline void AddToV(Size v);
+
+  inline bool IsAddedToV(Size v);
+
+  inline void Clear(Size *nec_distinct_cands, Size *num_nec_distinct_cands);
+
+ private:
+  const DataGraph &data_;
+  const QueryGraph &query_;
+  const CandidateSpace &cs_;
+  BacktrackHelper *backtrack_helpers_;
+
+  Size NIL = 0;
+  Size INF = std::numeric_limits<Size>::max();
+
+  Size *pair_U;
+  Size *pair_V;
+
+  Size *cand_to_v;
+
+  Size *dist;
+  Size *queue;
+
+  Size *nec_index;
+
+  Size size_U;
+  Size size_V;
+  Size max_label_counter;
+
+  inline bool BFS();
+  inline bool DFS(Size u);
+};
+
+inline MaximumMatching::MaximumMatching(const DataGraph &data,
+                                        const QueryGraph &query,
+                                        const CandidateSpace &cs,
+                                        BacktrackHelper *backtrack_helpers)
+    : data_(data),
+      query_(query),
+      cs_(cs),
+      backtrack_helpers_(backtrack_helpers) { /* code */ }
+
+inline MaximumMatching::~MaximumMatching() { /* code */ }
+
+inline Size MaximumMatching::ComputeMaximumMatching(Label label) { /* code */ }
+
+inline void MaximumMatching::AddToU(Size u) { /* code */ }
+
+inline void MaximumMatching::AddToV(Size v) { /* code */ }
+
+inline bool MaximumMatching::IsAddedToV(Size v) { /* code */ }
+
+inline void MaximumMatching::Clear(Size *nec_distinct_cands,
+                                   Size *num_nec_distinct_cands) { /* code */ }
+
+inline bool MaximumMatching::BFS() { /* code */ }
+
+inline bool MaximumMatching::DFS(Size u) { /* code */ }
+}  // namespace daf
+
+#endif  // MAXIMUM_MATCHING_H_
