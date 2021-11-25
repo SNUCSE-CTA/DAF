a00ce921f853ffcb83ce3757c4ceec50c32b7526
jihoonjang <ljjk159@gmail.com>
2021-11-25 19:36:40 +0900

    MatchLeaves implemented

--- src/match_leaves.cc
+++ src/match_leaves.cc
@@ -16,7 +16,22 @@ MatchLeaves::MatchLeaves(const DataGraph &data, const QueryGraph &query,
       cs_(cs),
       backtrack_mapped_query_vtx(mapped_query_vtx),
       backtrack_helpers_(helpers),
-      backtrack_mapped_nodes_(mapped_nodes) { /* code */ }
+      backtrack_mapped_nodes_(mapped_nodes) {
+  nec_distinct_cands_ = new Vertex[data_.GetNumVertices()];
+  cand_to_nec_idx_ = new std::vector<Size>[data_.GetNumVertices()];
+  nec_ranking_ = new Size[query_.GetNumNECLabel()];
+
+  sum_nec_cands_size_ = new Size[query_.GetNumNECLabel()];
+  sum_nec_size_ = new Size[query_.GetNumNECLabel()];
+
+  num_remaining_cands_ = new Size[query_.GetNumVertices()];
+  num_remaining_nec_vertices_ = new Size[query_.GetNumVertices()];
+
+  num_nec_distinct_cands_ = 0;
+
+  maximum_matching_ =
+      new MaximumMatching(data_, query_, cs_, backtrack_helpers_);
+}
 
 MatchLeaves::~MatchLeaves() { /* code */ }
 
